ebf9a06b3872df73b333c86b5fc505e708ea799e
wang <732539245@qq.com>
2021-01-16 18:04:44 +0800

    这是新增

--- src/components/spshuxing/SpShu.vue
+++ src/components/spshuxing/SpShu.vue
@@ -69,6 +69,53 @@
     </el-pagination>
   </div>
 
+  <!--新增模板-->
+  <div>
+    <el-dialog title="属性新增信息" :visible.sync="addForm">
+      <el-form :model="addData" ref="addData"  label-width="80px">
+        <el-form-item label="英文名称" prop="name">
+          <el-input v-model="addData.name" autocomplete="off" ></el-input>
+        </el-form-item>
+        <el-form-item label="中文名称" prop="nameCH">
+          <el-input v-model="addData.nameCH" autocomplete="off" ></el-input>
+        </el-form-item>
+        <el-form-item label="属性类型" prop="typeId">
+          <el-select v-model="addData.typeId" placeholder="请选择">
+            <el-option
+              v-for="item in bandData"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="类型" prop="type">
+          <el-select v-model="addData.type" placeholder="请选择">
+            <el-option
+              v-for="item in leixngData"
+              :key="item.type"
+              :label="item.name"
+              :value="item.type">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="SKU" prop="isSKU">
+          <el-switch
+            v-model="addData.isSKU"
+            active-text="是"
+            active-color="#13ce66"
+            :active-value="1"
+            inactive-text="否"
+            :inactive-value="0">
+          </el-switch>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="saveForm">确 定</el-button>
+        <el-button @click="addForm = false">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </div>
 </template>
 
@@ -90,31 +137,109 @@
             addForm:false,
             //查询出来的数据
             quydate:[],
+            //获取类型的数据
+            typeData:[],
+            //类型属性
+            bandData:[],
+            //类型下拉
+            leixngData:[{type:0,name:"下拉框"},{type:1,name:"单选框"},{type:2,name:"复选框"},{type:3,name:"输入框"}],
+            //新增的对象
+            addData:{
+              id:"",
+              name:"",
+              nameCH:"",
+              typeId:"",
+              type:"",
+              isSKU:""
+            },
+            rule:{ //验证规则
+              carname:[
+                { required: true, message: '请输入名称', trigger: 'blur' },
+                { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }
+              ]
+            }
         }
       } ,
       //初始化函数
       created:function () {
         this.queryDate();
+        this.queryType();
       },
-      methods:{
+      methods: {
         //第几页
-        handleCurrentChange:function(page){ //页数
-          this.param.start=page;
+        handleCurrentChange: function (page) { //页数
+          this.param.start = page;
           this.queryDate();
           //每页条数
-        },handleSizeChange:function(size){ //条数
-          this.param.size=size;
+        }, handleSizeChange: function (size) { //条数
+          this.param.size = size;
           this.queryDate();
         },
-          //查询的方法
-        queryDate:function () {
-          var par=this.$qs.stringify(this.param);
-          var url="http://localhost:8080/api/perpor/queryspoper?"+par;
-          this.$ajax.get(url).then(rs=>{
-            this.quydate=rs.data.data.list;
-            this.count=rs.data.data.count;
-          }).catch(er=>console.log(er))
-        }
+        //新增的方法
+        saveForm:function () {
+          this.$refs['addData'].validate(res=>{
+            if(res==true){
+              console.log(this.addData)
+              this.$ajax.post("http://localhost:8080/api/perpor/savespoper",this.$qs.stringify(this.addData)).then(rs=>{
+                this.addForm=false;
+                location.reload();
+              }).catch(er=>console.log(er));
+            }
+          });
+        },
+        //查询的方法
+        queryDate: function () {
+          var par = this.$qs.stringify(this.param);
+          var url = "http://localhost:8080/api/perpor/queryspoper?" + par;
+          this.$ajax.get(url).then(rs => {
+            this.quydate = rs.data.data.list;
+            this.count = rs.data.data.count;
+          }).catch(er => console.log(er))
+        },
+        //查询类型的方法
+        queryType: function () {
+          this.$ajax.get("http://localhost:8080/api/type/getData").then(res => {
+            this.typeData = res.data.data;
+            for (let i = 0; i < res.data.data.length; i++) {
+              if (res.data.data[i].pid == -1) {
+                this.diguiNode(res.data.data[i]);
+                break;
+              }
+            }
+          }).catch(err => console.log(err));
+        },
+        diguiNode: function (node) {
+          // 判断是否为父节点
+          var bf = this.isParent(node);
+          if (bf == true) {
+            for (let i = 0; i < this.typeData.length; i++) {
+              //判断是否为当前节点的子节点
+              if (node.id == this.typeData[i].pid) {
+                this.diguiNode(this.typeData[i]);
+              }
+            }
+          }
+          if (bf == false) {
+            this.bandData.push(node)
+          }
+        },
+        isParent: function (node) {// 判断是否为父节点  pid 有没有指向当前id
+          for (let i = 0; i < this.typeData.length; i++) {
+            if (node.id == this.typeData[i].pid) {
+              return true;
+            }
+          }
+          return false;
+      },
+        formaTypeId(row,column,value,index){
+          for (let i = 0; i <this.typeData.length; i++) {
+            if(value==this.typeData[i].id)
+            {return this.typeData[i].name}
+          }
+        },
+        SKU(row,column,value,index){
+          return value==1?"是":"否"
+        },
       }
     }
 </script>
