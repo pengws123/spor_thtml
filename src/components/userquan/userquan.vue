4394dc02de79f9fe155a7aa349e070f770b67bfb
wang <732539245@qq.com>
2021-02-07 17:24:10 +0800

    用户权限的修改和新增

--- src/components/userquan/userquan.vue
+++ src/components/userquan/userquan.vue
@@ -32,6 +32,65 @@
         </span>
       </span>
   </el-tree>
+
+  <!--新增-->
+  <el-dialog title="新增权限信息" :visible.sync="addFormFlag" width="500px">
+
+    <el-form :model="addForm" ref="addForm" label-width="80px">
+
+      <el-form-item label="名称" prop="name">
+        <el-input v-model="addForm.name" autocomplete="off" ></el-input>
+      </el-form-item>
+
+      <el-form-item label="路径" prop="url">
+        <el-input v-model="addForm.url" autocomplete="off" ></el-input>
+      </el-form-item>
+
+      <el-form-item label="类型:" prop="type">
+        <el-radio-group v-model="addForm.type">
+          <el-radio   :label="0">目录</el-radio>
+          <el-radio   :label="1">菜单</el-radio>
+        </el-radio-group>
+      </el-form-item>
+
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="addFormFlag = false">取 消</el-button>
+      <el-button type="primary" @click="addtijiao">确 定</el-button>
+    </div>
+
+  </el-dialog>
+
+
+  <!--修改-->
+  <el-dialog title="录入分类产品信息" :visible.sync="upFormFlag" width="500px">
+
+    <el-form :model="upForm" ref="upForm"  label-width="80px">
+      <el-input type="hidden" v-model="upForm.id" autocomplete="off" ></el-input>
+      <el-input type="hidden" v-model="upForm.pid" autocomplete="off" ></el-input>
+      <el-form-item label="名称" prop="name">
+        <el-input v-model="upForm.name" autocomplete="off" ></el-input>
+      </el-form-item>
+
+      <el-form-item label="路径" prop="url">
+        <el-input v-model="upForm.url" autocomplete="off" ></el-input>
+      </el-form-item>
+
+      <el-form-item label="类型:" prop="type">
+        <el-radio-group v-model="upForm.type">
+          <el-radio   :label="0">目录</el-radio>
+          <el-radio   :label="1">菜单</el-radio>
+        </el-radio-group>
+      </el-form-item>
+
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="upFormFlag = false">取 消</el-button>
+     <el-button type="primary" @click="uptijiao">确 定</el-button>
+    </div>
+
+  </el-dialog>
+
 </div>
 </template>
 
@@ -42,11 +101,56 @@
           return{
             data:[],//用来渲染树
             defaultProps: {},
+            addFormFlag:false,
+            upFormFlag:false,
             ajaxDate:[],//用来接收后台传来的值
             jsonstr:"",//用来 放进行拼接的值
+            addForm:{
+              pid:"",
+              name:"",
+              url:"",
+              type:""
+            },
+            upForm:{
+              id:"",
+              pid:"",
+              name:"",
+              url:"",
+              type:""
+            },
           }
       },
       methods:{
+        //修改的弹框
+        update:function (node, data) {
+          this.upFormFlag=true;
+          console.log(node);
+          console.log(data)
+          this.upForm.id=data.id;
+          this.upForm.pid=data.pid;
+          this.upForm.name=data.label;
+          this.upForm.url=data.url;
+          this.upForm.type=data.type;
+        },
+        //修改提交
+        uptijiao:function () {
+          this.$ajax.post("http://localhost:8080/api/xian/updateshore",this.$qs.stringify(this.upForm)).then(rs=>{
+            this.upFormFlag=false;
+            location.reload();
+          }).catch(er=>console.log(er))
+        },
+        //新增的弹框
+        append:function (data) {
+          this.addFormFlag=true;
+          this.addForm.pid=data.id;
+        },
+        //新增的提交
+        addtijiao:function () {
+          this.$ajax.post("http://localhost:8080/api/xian/saveshore",this.$qs.stringify(this.addForm)).then(rs=>{
+            this.addFormFlag=false;
+            location.reload();
+          }).catch(er=>console.log(er))
+        },
         //遍历赋值
         queryDate:function () {
           for (let i = 0; i <this.ajaxDate.length ; i++) {
@@ -62,7 +166,7 @@
           var isPar=this.isPared(obj);
           if(isPar==true){
             let co=0;
-            this.jsonstr+='{"id":'+obj.id+',"label":"'+obj.name+'","children":[';
+            this.jsonstr+='{"id":'+obj.id+',"pid":'+obj.pid+',"label":'+obj.name+',"url":"'+obj.url+'","type":'+obj.type+',"children":[';
             for (let i = 0; i <this.ajaxDate.length ; i++) {
               if(obj.id==this.ajaxDate[i].pid){
                 co++;
@@ -75,7 +179,7 @@
             }
             this.jsonstr+=']}';
           }else {
-            this.jsonstr+='{"id":'+obj.id+',"label":"'+obj.name+'"}';
+            this.jsonstr+='{"id":'+obj.id+',"pid":'+obj.pid+',"label":"'+obj.name+'","url":"'+obj.url+'","type":'+obj.type+'}';
           }
 
         },
