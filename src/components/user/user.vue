4e55735a45eb98543915bb319c6cc0988968116e
wang <732539245@qq.com>
2021-02-07 19:18:00 +0800

    用户回显的方法

--- src/components/user/user.vue
+++ src/components/user/user.vue
@@ -88,7 +88,96 @@
       </el-pagination>
     </div>
 
+    <!-- 修改  -->
+    <el-dialog title="用户信息" :visible.sync="upForm">
+      <el-form :model="upData" ref="upData"   label-width="80px">
 
+        <el-form-item label="名称" prop="name">
+          <el-col :span="11">
+            <el-input v-model="upData.name" autocomplete="off" ></el-input>
+          </el-col>
+        </el-form-item>
+
+
+        <el-form-item label="性别:" prop="sex">
+          <el-radio-group v-model="upData.sex">
+            <el-radio   :label="1">男</el-radio>
+            <el-radio   :label="2">女</el-radio>
+          </el-radio-group>
+        </el-form-item>
+
+        <el-form-item label="手机号" prop="phone">
+          <el-col :span="11">
+            <el-input v-model="upData.phone" autocomplete="off" ></el-input>
+          </el-col>
+        </el-form-item>
+
+        <el-form-item label="邮箱" prop="email">
+          <el-col :span="11">
+            <el-input v-model="upData.email" autocomplete="off" ></el-input>
+          </el-col>
+        </el-form-item>
+
+        <el-form-item label="身份证号" prop="idCard">
+          <el-col :span="11">
+            <el-input v-model="upData.idCard" autocomplete="off" ></el-input>
+          </el-col>
+        </el-form-item>
+
+        <el-form-item label="时间" prop="birthday">
+          <el-date-picker
+            v-model="upData.birthday"
+            type="date"
+            value-format="yyyy-MM-dd"
+            placeholder="选择日期">
+          </el-date-picker>
+        </el-form-item>
+
+        <el-form-item label="头像">
+         <img  width="50px" :src="upData.imgUrl"/>
+          <el-upload
+            class="upload-demo"
+            action="http://localhost:8080/api/name/updateFile"
+            :on-success="imgCallBack"
+            name="file"
+            list-type="picture">
+            <el-button size="small" type="primary">点击上传</el-button>
+            <div slot="tip" class="el-upload__tip"></div>
+          </el-upload>
+        </el-form-item>
+
+        <el-form-item label="学历" prop="eduId">
+          <el-select v-model="upData.eduId" placeholder="请选择">
+            <el-option
+              v-for="item in eduData"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+
+        </el-form-item>
+
+        <el-form-item label="部门" prop="deprId">
+          <el-select v-model="upData.deprId" placeholder="请选择">
+            <el-option
+              v-for="item in depData"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+
+        </el-form-item>
+
+
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addForm = false">取 消</el-button>
+       <!-- <el-button type="primary" @click="saveForm">确 定</el-button>-->
+      </div>
+
+    </el-dialog>
   </div>
 </template>
 
@@ -103,13 +192,29 @@
             size:2,
             start:1
           },
+          //修改的对象
+          upData:{},
+          //修改信息的弹框属性
+          upForm:false,
           //总条数
           count:0,
           //查询出来的数据
           quydate:[],
+          //学历下拉
+          eduData:[{"value":1,"label":"高中"},{"value":2,"label":"大专"},{"value":3,"label":"本科"}],
+          //部门下拉
+          depData:[{"value":1,"label":"人事"},{"value":2,"label":"全粘"},{"value":3,"label":"完全粘"}],
         }
       },
       methods:{
+        //修改用户的弹框
+        showupdate:function(index,row){
+          this.upForm=true;
+          var url="http://localhost:8080/api/user/selectuser?id="+row.id;
+          this.$ajax.post(url).then(rs=>{
+            this.upData =rs.data.data;
+          }).catch(er=>console.log(er))
+        },
         //删除用户的方法
         dele:function(index,row){
           var url="http://localhost:8080/api/user/deleteuser?id="+row.id;
@@ -125,7 +230,12 @@
         }, handleSizeChange: function (size) { //条数
           this.param.size = size;
           this.queryDate();
-        },//查询的方法
+        },
+        //图片
+        imgCallBack:function(response, file, fileList){
+          this.upData.imgUrl=response.data;
+        },
+        //查询的方法
         queryDate: function () {
           var par = this.$qs.stringify(this.param);
           var url = "http://localhost:8080/api/user/queryuser?" + par;
@@ -140,26 +250,16 @@
         },
         //处理学历
         xueli(row,column,value,index){
-          if(value==1){
-            return "高中"
-          }
-          if(value==2){
-            return "大专"
-          }
-          if(value==3){
-            return "本科"
+          for (let i = 0; i <this.eduData.length; i++) {
+            if(value==this.eduData[i].value)
+            {return this.eduData[i].label}
           }
         },
         //处理部门
         bumen(row,column,value,index){
-          if(value==1){
-            return "人事"
-          }
-          if(value==2){
-            return "全粘"
-          }
-          if(value==3){
-            return "完全粘"
+          for (let i = 0; i <this.depData.length; i++) {
+            if(value==this.depData[i].value)
+            {return this.depData[i].label}
           }
         }
       },//初始化函数
